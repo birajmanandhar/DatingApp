af3e00fd21b19e08c2a3cbc7cd3eee9d6a40c2e3
birajmanandhar <biraj.manandhar@gmail.com>
2023-09-07 17:08:38 +1200

    Error handling, interceptor and relevant pages

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using API.ApplicationServiceExtensions;
 using API.Extensions;
+using API.Middleware;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
